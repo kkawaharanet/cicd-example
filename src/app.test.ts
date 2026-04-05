0cf7a9c79c6410afc43372553b6717b6499fee60
kkawaharanet <kkawaharanet@gmail.com>
2026-04-05 17:42:33 +0900

    リファクタリングした

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, test } from "vitest";
 import { app } from "./app.js";
 
-describe("app", () => {
-  test("計算結果を得られること", async () => {
+describe("足し算API", () => {
+  test("正しい足し算の結果を得られること", async () => {
     const response = await app.request("/sum");
 
     // HTTPステータスコードが200であること
     expect(response.status).toBe(200);
 
-    // 計算結果が期待値と一致すること
+    // 足し算の結果が期待値と一致すること
     const body = await response.json();
     expect(body).toEqual({ result: 2 });
   });
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import { Hono } from "hono";
 import { sum } from "./sum.js";
 
-export const app = new Hono().get("/sum", (c) => {
+export const app = new Hono();
+
+/**
+ * 足し算API
+ */
+app.get("/sum", (c) => {
   return c.json({ result: sum(1, 1) });
 });
